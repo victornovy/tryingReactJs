1e96c52fc5a40f397408e875f8673fd861dd5b41
Victor Novy <novyvictor@gmail.com>
2016-10-25 23:49:14 -0200

    Create button using material-ui and chenge size button in runtime

--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TextField from 'material-ui/TextField';
+import {deepOrange500, blue900, teal50} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(
-    <h1>Test</h1>,
-    document.getElementById('example')
-);
+injectTapEventPlugin();
+
+const style = {
+  marginRight: 20,
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+    primary1Color: blue900,
+    canvasColor:teal50,
+  },
+});
+
+
+class Layout extends React.Component {
+
+    state = {
+        isMini: true
+    };
+
+    changeMiniState = function(isMini) {
+        this.setState({isMini: !isMini});
+    };
+
+    render() {
+        return(
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <div>
+                    <h1>It works!</h1>
+                    <TextField hintText="Hint Text" />
+                    <br />
+                    <FloatingActionButton mini={this.state.isMini} style={style} onTouchTap={() => this.changeMiniState(this.state.isMini)}>
+                        <ContentAdd />
+                    </FloatingActionButton>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
+
+const example = document.getElementById('example');
+
+ReactDOM.render(<Layout />, example);
