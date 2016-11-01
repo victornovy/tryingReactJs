cfe876b63120487b0ff0e511bbbfd61f62e0231a
Victor Novy <novyvictor@gmail.com>
2016-10-31 23:04:15 -0200

    Intro to single page apps

---
+++ src/components/singlePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class SinglePage extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Single Page</h1>
+                <Link to="event"><button>Event</button></Link>
+                <Link to="stateXProps"><button>State X Props</button></Link>
+                <Link to="material"><button>Material</button></Link>
+                {this.props.children}
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-import Material from './components/materialUI'
-import StateXProps from './components/stateXProps'
-import Event from './components/event'
+import Material from './components/materialUI';
+import StateXProps from './components/stateXProps';
+import Event from './components/event';
+import SinglePage from './components/singlePage';
 
 const example = document.getElementById('example');
 
-ReactDOM.render(<Event />, example);
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={SinglePage}>
+            <IndexRoute component={Material}></IndexRoute>
+            <Route path="event" component={Event}></Route>
+            <Route path="stateXProps" component={StateXProps}></Route>
+            <Route path="material" component={Material}></Route>
+        </Route>
+    </Router>,
+    example
+);
